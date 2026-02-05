6253a5e7402253a454946b7343f0c61413f7f96f
DEVSING2004 <devsinghpal007@gmail.com>
2026-02-05 22:16:45 +0530

    setup complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <h2>DEV SINGH</h2>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
